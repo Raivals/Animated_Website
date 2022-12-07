0440ffde2537e614840fefa733f7244f06abe60f
Raivals <103427644+Raivals@users.noreply.github.com>
2022-12-07 21:38:23 +0100

    Structure Navbar

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react"
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <section className="res-navbar">
+        <div className="topp-area">
+          <div className="nav-row">
+            <div className="column">
+              <div className="social-icon"></div>
+            </div>
+            <div className="column"></div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  )
+}
+
+export default Navbar
