8e866d24c8fa060e700e0121ec3fd7429c472c49
Raivals <103427644+Raivals@users.noreply.github.com>
2022-12-08 11:38:25 +0100

    Init Services

---
+++ src/Component/Services.jsx
@@ -0,0 +1,61 @@
+import React, { Fragment } from "react"
+
+const Services = () => {
+  return (
+    <Fragment>
+      <section className="services">
+        <div className="heading">
+          <h3>Explore Our Solutions</h3>
+        </div>
+        <div className="services-row">
+          <div className="column">
+            <div className="hover">
+              <h3>Timing</h3>
+              <div className="hover-content">
+                <div className="hover-content-inner">
+                  <h4>Timing</h4>
+                  <p>
+                    Shingle color was not something the couple had ever talked
+                    about.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="hover">
+              <h3>Business Growth</h3>
+              <div className="hover-content">
+                <div className="hover-content-inner">
+                  <h4>Business Growth</h4>
+                  <p>
+                    Shingle color was not something the couple had ever talked
+                    about.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="hover">
+              <h3>Problem Solving</h3>
+              <div className="hover-content">
+                <div className="hover-content-inner">
+                  <h4>Problem Solving</h4>
+                  <p>
+                    Shingle color was not something the couple had ever talked
+                    about.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  )
+}
+
+export default Services
