c014bc949b13f8a497906ee3537e8c049c320b21
Raivals <103427644+Raivals@users.noreply.github.com>
2022-12-08 11:39:28 +0100

    Init Services

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css"
 import React, { Fragment } from "react"
 import Navbar from "./Component/Navbar"
 import Home from "./Component/Home"
+import Services from "./Component/Services"
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
       <Home />
+      <Services />
     </Fragment>
   )
 }
